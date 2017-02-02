74ec84f0628c18d6e1adb978168683d7c4c649e8
Alice Jiang <kiki86722@gmail.com>
2017-02-02 15:28:12 +0800

    modify Hello

--- Hello.java
+++ Hello.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] argv) {
         Scanner input = new Scanner(System.in);
-        System.out.println("*");
-        int i;
+        // System.out.println("Hello World");
+        // int i, j;
         int n = input.nextInt();
-        int result = 1;
-        for (i=1; i <= n; i++) {
-            result = result + 1;
-            System.out.println("*");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+              System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
